49a7d49b10d628c3d329ded264f9f07f96b00fdf
amorgiel <anna.morgiel@scout24.com>
2023-10-31 22:00:00 +0100

    set UI

--- app/src/main/java/com/example/mygraphql/MainActivity.kt
+++ app/src/main/java/com/example/mygraphql/MainActivity.kt
@@ -3,44 +3,20 @@ package com.example.mygraphql
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.mygraphql.ui.GitHubViewModel
+import com.example.mygraphql.ui.composable.GitHubScreen
 import com.example.mygraphql.ui.theme.MyGraphQLTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyGraphQLTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                GitHubScreen(viewModel = hiltViewModel<GitHubViewModel>())
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyGraphQLTheme {
-        Greeting("Android")
     }
 }
